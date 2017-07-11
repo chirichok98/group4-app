94900da12621450d6176e34cd4f21486e85b61ad
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-11 15:45:28 +0300

    navbar panel added

---
+++ src/app/components/navbar/navbar.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent],
+})
+export class NavbarModule { }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home.state';
 import { HomeComponent } from './home.component';
 import { LoginFormModule } from '../../components/login/login.module';
+import { NavbarModule } from '../../components/navbar/navbar.module';
 // add HomeService
 
 @NgModule({
@@ -12,6 +13,7 @@ import { LoginFormModule } from '../../components/login/login.module';
   imports: [
     HomeRoutingModule,
     LoginFormModule,
+    NavbarModule,
   ],
   providers: [/*HomeService*/],
 })
