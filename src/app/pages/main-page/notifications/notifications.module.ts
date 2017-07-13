8c774de3be22f56e207b1397e8113bce443a562b
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-13 13:37:22 +0300

    notifications component added

---
+++ src/app/pages/main-page/notifications/notifications.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { NotificationsComponent } from './notifications.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [NotificationsComponent],
+  providers: [],
+})
+export class NotificationsModule { }
