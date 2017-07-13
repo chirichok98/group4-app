f5dd525dba3b365de9d2301fa5c2fc68e028444e
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-13 13:37:50 +0300

    candidates component added

---
+++ src/app/pages/main-page/candidates/candidates.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'candidates',
+  templateUrl: 'candidates.component.html',
+})
+
+export class CandidatesComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/app/pages/main-page/candidates/candidates.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { CandidatesComponent } from './candidates.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [CandidatesComponent],
+  providers: [],
+})
+export class CandidatesModule { }
