d6c9de176454f7aac673c86c1556516d65f993e9
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-13 18:04:39 +0300

    filter component added

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-panel',
+  templateUrl: 'filter.component.html',
+})
+
+export class FilterComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
