4feb2b4074e36d39b5a3a42f478948b9854629c0
Max <17.max.starling@gmail.com>
2017-07-14 17:22:37 +0300

    imgs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,15 +14,15 @@ export class HomeComponent {
   descriptions: IDescribeBlock[] = [
     {
       title: 'Сontemporary', content: `Only modern and high-quality technologies have been used for development, which simplifies the work with the system in the near future.`,
-      imageUrl: './assets/opportunities/buildings_medium2.png',
+      imageUrl: './assets/opportunities/buildings3.png',
     },
     {
       title: 'Minimalistic', content: `Popular, comfortable and familiar design.`,
-      imageUrl: `./assets/opportunities/minimalism_medium.png`,
+      imageUrl: `./assets/opportunities/minimalistic/minimalistic3.png`,
     },
     {
       title: 'Secure', content: `We were able to provide a sufficient level of security. So, the company's employees may not worry.`,
-      imageUrl: `./assets/opportunities/lock_medium.png`,
+      imageUrl: `./assets/opportunities/lock4.png`,
     },  
   ];
   
