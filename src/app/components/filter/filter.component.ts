ef880db54b25764432a8838939613507d42dd7a6
Max Starling <17.max.starling@gmail.com>
2017-07-15 23:02:27 +0300

    beautiful selection. filter & search blocks was added. new filter & search icons. opportunities hover scale. small fixes.

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'filter-panel',
   templateUrl: 'filter.component.html',
+  styleUrls: ['./filter.component.scss'],
 })
 
 export class FilterComponent implements OnInit {
   constructor() { }
-
   ngOnInit() { }
 }
