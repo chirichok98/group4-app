3e87da04705c8a251f6e28a9d5c4fa7e345ae7df
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-17 12:02:19 +0300

    candidate-card handler for detail view added

--- src/app/components/candidate-card/candidate-card.component.ts
+++ src/app/components/candidate-card/candidate-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface ICandidatePreview {
   name: string;
@@ -12,11 +13,17 @@ export interface ICandidatePreview {
   templateUrl: './candidate-card.component.html',
   styleUrls: ['./candidate-card.component.scss'],
 })
-export class CandidateCardComponent implements OnInit{
+export class CandidateCardComponent implements OnInit {
   @Input() candidateItem: ICandidatePreview;
   curCandidate: ICandidatePreview;
 
+  constructor(private router: Router) { }
+
   ngOnInit() {
     this.curCandidate = this.candidateItem;
+  }
+
+  goToDetailView() {
+    this.router.navigate(['main-page/candidates', this.curCandidate.name]);
   }
 }
