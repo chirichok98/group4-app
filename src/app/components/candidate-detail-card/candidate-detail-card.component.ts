fa76b63523e7b5049217120e2dc7f7cbfa999639
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-17 15:16:21 +0300

    candidate-detail-card added

---
+++ src/app/components/candidate-detail-card/candidate-detail-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'candidate-detail-card',
+  templateUrl: 'candidate-detail-card.component.html',
+  styleUrls: ['candidate-detail-card.component.scss'],
+})
+export class CandidateDetailCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/app/components/candidate-detail-card/candidate-detail-card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { CandidateDetailCardComponent } from './candidate-detail-card.component';
+
+@NgModule({
+  imports: [],
+  exports: [CandidateDetailCardComponent],
+  declarations: [CandidateDetailCardComponent],
+  providers: [],
+})
+export class CandidateDetailCardModule { }
