409e6448cba8aee2f23eba8c00eebbe2aff78703
Evgeniya Severinets <fad5@yandex.ru>
2017-07-17 12:23:54 +0300

    created notification component

--- src/app/pages/main-page/notifications/notifications.component.ts
+++ src/app/pages/main-page/notifications/notifications.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'notifications',
   templateUrl: 'notifications.component.html',
+  styleUrls: ['notifications.component.scss'],
 })
 
 export class NotificationsComponent implements OnInit {
