ff9c6a918084f7f4d7096660490d9efff6292883
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-17 15:18:23 +0300

    candidates-list-item upgraded

--- src/app/pages/main-page/candidates/candidates-list-item/candidates-list-item.module.ts
+++ src/app/pages/main-page/candidates/candidates-list-item/candidates-list-item.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { CandidatesListItemComponent } from './candidates-list-item.component';
+// tslint:disable-next-line:max-line-length
+import { CandidateDetailCardModule } from '../../../../components/candidate-detail-card/candidate-detail-card.module';
 
 @NgModule({
-  imports: [],
+  imports: [CandidateDetailCardModule],
   exports: [],
   declarations: [CandidatesListItemComponent],
   providers: [],
