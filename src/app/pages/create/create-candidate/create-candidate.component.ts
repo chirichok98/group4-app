508740ec67e8f39624a257ce4d2051a1dcee6195
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-20 16:30:38 +0300

    create-candidate component added

---
+++ src/app/pages/create/create-candidate/create-candidate.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-candidate',
+  templateUrl: 'create-candidate.component.html',
+  styleUrls: ['create-candidate.component.scss'],
+})
+export class CreateCandidateComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
