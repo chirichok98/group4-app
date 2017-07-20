a439b9aa2bfc1e4e8c7632e620e0c1e2bcc8990c
Evgeniya Severinets <fad5@yandex.ru>
2017-07-20 13:30:52 +0300

    action button menu added

---
+++ src/app/components/action-button-menu/action-button-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'action-button-menu',
+    templateUrl: 'action-button-menu.component.html',
+    styleUrls: ['action-button-menu.component.scss'],
+})
+
+export class ActionButtonComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
