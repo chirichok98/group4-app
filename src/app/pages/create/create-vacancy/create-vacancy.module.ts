fef2d8831b4afe656629e2f0c8c833a22b6633cf
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-20 16:30:21 +0300

    create-vacancy module added

---
+++ src/app/pages/create/create-vacancy/create-vacancy.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { CreateVacancyComponent } from './create-vacancy.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [CreateVacancyComponent],
+  providers: [],
+})
+export class CreateVacancyModule { }
