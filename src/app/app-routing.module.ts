d18ddda916e913221bde8f767d6163a68e7aad5b
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-20 16:33:54 +0300

    navigation for create components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 
 import { ReportComponent } from './pages/report/report.component';
+// tslint:disable-next-line:max-line-length
+import { CreateCandidateComponent } from './pages/create/create-candidate/create-candidate.component';
+import { CreateVacancyComponent } from './pages/create/create-vacancy/create-vacancy.component';
+
+const mainRoute: Route = {
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full',
+};
+const reportRoute: Route = {
+  path: 'report',
+  component: ReportComponent,
+};
+const creatingRoute: Route = {
+  path: 'create',
+  children: [
+    {
+      path: '',
+      redirectTo: 'candidate',
+      pathMatch: 'full',
+    },
+    {
+      path:'candidate',
+      component: CreateCandidateComponent,
+    },
+    {
+      path:'vacancy',
+      component: CreateVacancyComponent,
+    },
+  ],
+};
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'report',
-    component: ReportComponent,
-  },
+  mainRoute,
+  reportRoute,
+  creatingRoute,
 ];
 
 @NgModule({
