34aab58d614cefb442aeaa1ecfbacf7f64ba637a
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-24 21:31:58 +0300

    help-service added

--- src/app/pages/create/create-candidate/create-candidate.module.ts
+++ src/app/pages/create/create-candidate/create-candidate.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { CreateCandidateComponent } from './create-candidate.component';
-import { CreateCandidateService, HelpService } from './create-candidate.service';
+import { CreateCandidateService } from './create-candidate.service';
 import { FormsModule } from '@angular/forms';
 import { ContactsFormModule } from '../../../components/contacts-form/contacts-form.module';
 import { PrevJobFormModule } from '../../../components/prev-job-form/prev-job-form.module';
 import { CommonModule } from '@angular/common';
 import { SelectFormModule } from '../../../components/select-form/select-form.module';
 import { DatepickerFormModule } from '../../../components/datepicker-form/datepicker-form.module';
+import { HelpService } from '../help.service';
 
 @NgModule({
   imports: [
