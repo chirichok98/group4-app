24b4ffa0f356871736af7a90097442e52b2e96c1
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-24 10:33:14 +0300

    spinner for position-fdetail view added, default picture for skill added

--- src/app/components/position-detail-card/position-detail-card.module.ts
+++ src/app/components/position-detail-card/position-detail-card.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PositionDetailComponent } from './position-detail-card.component';
+import { SpinnerModule } from '../spinner/spinner.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SpinnerModule,
+  ],
   exports: [PositionDetailComponent],
   declarations: [PositionDetailComponent],
   providers: [],
