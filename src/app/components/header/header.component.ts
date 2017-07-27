27f3bc05cb15cd191f5eac36375f9d1a2f5fb673
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-27 18:14:36 +0300

    header upgraded

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { IUserPreview } from '../../interfaces/IUserPreview';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent { 
-  loggedUser: IUserPreview = {
-    id: 1,
-    name: 'Vasiliy Tovarishev',
-    role: 'admin',
-    picture: null,
-  };
+export class HeaderComponent {
+  loggedUser: any;
+
+  constructor(private cookie: CookieService) {
+    console.log('get user');
+    this.loggedUser = this.cookie.getObject('current');
+  }
+
+  ngDoCheck() {
+    this.loggedUser = this.cookie.getObject('current');
+  }
 }
--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
 import { ActionButtonModule } from '../action-button-menu/action-button-menu.module';
+import { CookieModule } from 'ngx-cookie';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [HeaderComponent],
+  imports: [
+    CommonModule,
+    ActionButtonModule,
+    CookieModule.forChild(),
+  ],
   exports: [HeaderComponent],
   providers: [],
-  imports: [ActionButtonModule],
 })
 export class HeaderModule { }
