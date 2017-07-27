08ffb8f53e467d89ce7f3828c0afd3773cd25dc8
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-27 18:13:37 +0300

    action-button to user connected

--- src/app/components/action-button-menu/action-button-menu.component.ts
+++ src/app/components/action-button-menu/action-button-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IUserPreview } from '../../interfaces/IUserPreview';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'action-button-menu',
@@ -11,11 +12,16 @@ import { Router } from '@angular/router';
 export class ActionButtonComponent implements OnInit {
   @Input() user: IUserPreview;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private cookie: CookieService) { }
 
   ngOnInit() { }
 
-  openPage(url) {
+  openPage(url): void {
     this.router.navigate([url]);
+  }
+
+  logout(): void {
+    this.cookie.remove('current');
+    this.router.navigate(['home']);
   }
 }
