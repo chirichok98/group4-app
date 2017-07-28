64d8316028e02ac13b19b00cacba2135a134025a
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-28 21:56:51 +0300

    contacts form updated for material

--- src/app/components/contacts-form/contacts-form.module.ts
+++ src/app/components/contacts-form/contacts-form.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { ContactsFormComponent } from './contacts-form.component';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MaterialModule,
+  ],
   exports: [ContactsFormComponent],
   declarations: [ContactsFormComponent],
   providers: [],
