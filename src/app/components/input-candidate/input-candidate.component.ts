e2cb4782b0e44e97bb0ccb5b3b847bf981e77104
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-29 01:32:10 +0300

    input-candidate form added

---
+++ src/app/components/input-candidate/input-candidate.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, EventEmitter, Output, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
+import { CreateCandidateService } from '../../services/create-candidate.service';
+import { DictionariesService } from '../../services/dictionaries.service';
+import { IGeneral } from '../../interfaces/IGeneral';
+
+@Component({
+  selector: 'input-candidate',
+  templateUrl: 'input-candidate.component.html',
+  styleUrls: ['input-candidate.component.scss'],
+})
+export class InputCandidateComponent implements DoCheck {
+  @Input() canInfo: any;
+  @Output() canInfoChange: EventEmitter<any> = new EventEmitter<any>();
+
+  statuses: IGeneral[] = [];
+  cities: IGeneral[] = [];
+  englishLevel: IGeneral[] = [];
+  skills: IGeneral[] = [];
+
+  constructor(private router: Router,
+              private ccService: CreateCandidateService,
+              private hService: DictionariesService) {
+    this.hService.getCities().then((cities) => {
+      this.cities = cities;
+    });
+    this.hService.getEnglishLevel().then((levels) => {
+      this.englishLevel = levels;
+    });
+    this.hService.getSkills().then((skills) => {
+      this.skills = skills;
+    });
+    this.hService.getCandidateStatuses().then((statuses) => {
+      this.statuses = statuses;
+    });
+  }
+
+  ngDoCheck(): void {
+    console.log(this.canInfo);
+    this.canInfoChange.emit(this.canInfo);
+  }
+
+  addPrevJob(): void {
+    this.canInfo.candidatePrevJobs.push({ contact: {} });
+  }
+
+  removePrevJob(): void {
+    this.canInfo.candidatePrevJobs.pop();
+  }
+
+  addSecondary() {
+    this.canInfo.candidateSecondarySkills.push({});
+  }
+
+  removeSecondary() {
+    this.canInfo.candidateSecondarySkills.pop();
+  }
+}
