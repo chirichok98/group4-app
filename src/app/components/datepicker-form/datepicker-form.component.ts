9897e0e697fccefe97e8f839e134ebe9170e8354
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-28 23:43:55 +0300

    datepicker with material upgraded

--- src/app/components/datepicker-form/datepicker-form.component.ts
+++ src/app/components/datepicker-form/datepicker-form.component.ts
@@ -7,38 +7,19 @@ declare const $;
   templateUrl: 'datepicker-form.component.html',
   styleUrls: ['datepicker-form.component.scss'],
 })
-export class DatepickerFormComponent implements DoCheck {
+export class DatepickerFormComponent {
   @Input() placeholder: string;
+  
+  minDate = new Date(2010, 0, 1);
+  maxDate = new Date(2040, 0, 1);
 
-  minDate = new Date(2000, 0, 1);
-  maxDate = new Date(2020, 0, 1);
-    value = new Date(2020, 0, 1);
-
-  date = new Date(2000, 0, 1);
+  @Input() date: Date;
   @Output() dateChange: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  // parseDate(dateString: string): Date {
-  //   console.log('here');
-  //   if (dateString) {
-  //     this.dateChange.emit(this.date);
-  //     return new Date(dateString);
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
-  ngDoCheck() {
-    // console.log(this.value);
-    // this.dateChange.emit(this.date);
-    // console.log('emit');
-  }
-
-  dateChanged(date) {
+  onChange(date: any) {
+    this.dateChange.emit(date);
     console.log(date);
   }
 }
