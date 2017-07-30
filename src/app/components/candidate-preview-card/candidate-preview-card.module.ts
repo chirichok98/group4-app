db4fdc98be022449454b665881b3b11dc9bc32a5
Evgeniya Severinets <fad5@yandex.ru>
2017-07-30 03:17:00 +0300

    candidate-detail-card to material changed

--- src/app/components/candidate-preview-card/candidate-preview-card.module.ts
+++ src/app/components/candidate-preview-card/candidate-preview-card.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CandidatePreviewComponent } from './candidate-preview-card.component';
 import { SpinnerModule } from '../spinner/spinner.module';
+import {MdCardModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     SpinnerModule,
+    MdCardModule,
   ],
   declarations: [CandidatePreviewComponent],
   exports: [CandidatePreviewComponent],
