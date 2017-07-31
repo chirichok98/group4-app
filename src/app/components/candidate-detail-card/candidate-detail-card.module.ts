2dd64322ae6ded6783bff01a655834d12942d1b9
Mikhail.Chirich <chirichok98@gmail.com>
2017-07-31 16:42:27 +0300

    candidate detail chips added

--- src/app/components/candidate-detail-card/candidate-detail-card.module.ts
+++ src/app/components/candidate-detail-card/candidate-detail-card.module.ts
@@ -4,12 +4,14 @@ import { CandidateDetailComponent } from './candidate-detail-card.component';
 import { CommonModule } from '@angular/common';
 import { PositionPreviewModule } from '../position-preview-card/position-preview-card.module';
 import { SpinnerModule } from '../spinner/spinner.module';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     SpinnerModule,
     PositionPreviewModule,
+    MaterialModule,
   ],
   exports: [CandidateDetailComponent],
   declarations: [CandidateDetailComponent],
