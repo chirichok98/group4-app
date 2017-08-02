fe8019f466eef3404513590e932bdc303ab06fa3
Max <17.max.starling@gmail.com>
2017-08-02 18:22:43 +0300

    search & pagination was linked.

--- src/app/components/search/search.module.ts
+++ src/app/components/search/search.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { SearchComponent} from './search.component';
+import { SearchComponent } from './search.component';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
 import { SelectFormModule } from '../select-form/select-form.module';
