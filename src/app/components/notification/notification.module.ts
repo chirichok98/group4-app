a562b1b94ce565cfb5a0fdea857677155ccbc8be
Evgeniya Severinets <fad5@yandex.ru>
2017-08-05 16:51:33 +0300

    redesigned notifications

--- src/app/components/notification/notification.module.ts
+++ src/app/components/notification/notification.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { InterviewFeedbackComponent } from '../interview-feedback/interview-feedback.component';
 import { FormsModule } from '@angular/forms';
 import { InterviewFeedbackModule } from '../interview-feedback/interview-feedback.module';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     InterviewFeedbackModule,
+    MaterialModule,
   ],
   exports: [NotificationComponent],
   declarations: [NotificationComponent],
