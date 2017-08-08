0ecf8ac976270d7629824bdfb24be8860553b050
Evgeniya Severinets <fad5@yandex.ru>
2017-08-08 13:52:57 +0300

    fix snackbar open time

--- src/app/services/snackbar.service.ts
+++ src/app/services/snackbar.service.ts
@@ -7,7 +7,7 @@ export class SnackbarService {
 
   showSnack(message: string, action?: string) {
     this.snackBar.open(message, action, {
-      duration: 150000,
+      duration: 1500,
     });
   }
 }
